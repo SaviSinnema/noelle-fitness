5b02e846f0c454e52332f66d9f5162070107c4bd
Anneke Sinnema <mail@annekesinnema.nl>
2024-01-22 00:45:04 +0100

    Added Wordpress theme, ACF setup, plugins
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

---
+++ wordpress-thema/noelle-rodriguez/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package noelle-rodriguez
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<span class="sep"> | </span>
+				<!-- <?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'noelle-rodriguez' ), 'noelle-rodriguez', '<a href="https://www.annekesinnema.nl">Anneke Sinnema</a>' );
+				?> -->
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wordpress-thema/noelle-rodriguez/page-homepage.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Template Name: Homepage 
+ * 
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package noelle-rodriguez
+ */
+
+get_header();
+?>
+
+<?php if( get_field('general_logo') ): 
+    $general_logo = get_field('general_logo'); //acf field name
+    if( !empty( $general_logo ) ): ?>
+    <img src="<?php echo esc_url($general_logo['url']); ?>" alt="<?php echo esc_attr($general_logo['alt']); ?>" />
+<?php endif; endif; ?>
+
+<?php if( get_field('header_photo') ): 
+    $header_photo = get_field('header_photo'); //acf field name
+    if( !empty( $header_photo ) ): ?>
+    <img src="<?php echo esc_url($header_photo['url']); ?>" alt="<?php echo esc_attr($header_photo['alt']); ?>" />
+<?php endif; endif; ?>
+
+<?php if( get_field('cta_form') ): the_field('cta_form'); endif; ?>
+
+<hr>
+
+<?php if( have_rows('steps') ): 
+    while( have_rows('steps') ): the_row(); 
+?>
+
+    <h2><?php the_sub_field('steps_title'); ?></h2>
+
+    <ol>
+
+    <?php if( have_rows('step_1') ): 
+        while( have_rows('step_1') ): the_row(); 
+    ?>
+        <li>
+            <h3><?php the_sub_field('step_1_title'); ?></h3>
+            <?php the_sub_field('step_1_description'); ?>
+        </li>
+    <?php endwhile; endif; ?>
+
+    <?php if( have_rows('step_2') ): 
+        while( have_rows('step_2') ): the_row(); 
+    ?>
+        <li>
+            <h3><?php the_sub_field('step_2_title'); ?></h3>
+            <?php the_sub_field('step_2_description'); ?>
+        </li>
+    <?php endwhile; endif; ?>
+
+    <?php if( have_rows('step_3') ): 
+        while( have_rows('step_3') ): the_row(); 
+    ?>
+        <li>
+            <h3><?php the_sub_field('step_3_title'); ?></h3>
+            <?php the_sub_field('step_3_description'); ?>
+        </li>
+    <?php endwhile; endif; ?>
+    
+    </ol>
+
+<?php endwhile; endif; ?>
+
+<hr>
+
+
+<?php if( have_rows('the_program_includes') ): 
+    while( have_rows('the_program_includes') ): the_row(); 
+        if( have_rows('call_to_action') ): 
+        while( have_rows('call_to_action') ): the_row(); 
+    ?>
+
+        <p>
+            <?php the_sub_field('call_to_action'); ?>
+        </p>
+        <a href="<?php the_sub_field('call_to_action_link'); ?>">
+            <?php the_sub_field('call_to_action_link_microcopy'); ?>
+        </a>
+
+    <?php endwhile; 
+        endif; 
+    ?>
+
+    <?php the_sub_field('benefits_part_1'); ?>
+    <?php the_sub_field('benefits_part_2'); ?>
+
+    <?php 
+    $benefits_photo = get_field('benefits_photo'); //acf field name
+    if( !empty( $benefits_photo ) ): ?>
+        <img src="<?php echo esc_url($benefits_photo['url']); ?>" alt="<?php echo esc_attr($benefits_photo['alt']); ?>" />
+    <?php endif; ?>
+    
+<?php 
+endwhile;
+endif; ?>
+
+<hr>
+
+<?php if( get_field('inspirational_slogan') ): ?>
+    International slogan: <?php the_field('inspirational_slogan'); ?>
+<?php endif; ?>
+
+<hr>
+
+<?php if( have_rows('about_noelle') ): 
+    while( have_rows('about_noelle') ): the_row(); 
+?>
+    <?php 
+        $picture_in_footer = get_sub_field('picture_in_footer'); //acf field name
+        if( !empty( $picture_in_footer ) ): ?>
+        <img src="<?php echo esc_url($picture_in_footer['url']); ?>" alt="<?php echo esc_attr($picture_in_footer['alt']); ?>" />
+    <?php endif; ?>
+
+    <?php the_sub_field('about_noelle_text'); ?>
+<?php endwhile; endif; ?>
+
+<?php
+// get_sidebar();
+// get_footer();
